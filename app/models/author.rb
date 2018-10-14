c36e637569194ad55421377c55bb78bbb0234435
DDarrow123 <ddarrowphl@gmail.com>
2018-10-14 16:40:55 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,13 @@
 class Author < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :phone_number, length: { is: 10 }
+  validates :name, uniqueness: true
+  
 end
+
+Author.create(name: "John Doe").valid? # => true
+Author.create(name: nil).valid?
+
+# Author.create(phone_number: 555305442).valid? # => true
+# Author.create(phone_number: nil).valid?
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+
+  validate :title_is_present
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    # , message: "%{value} is not a valid size" }
+
+    def title_is_present
+      if !title.present?
+        errors.add(:title, "title can't be empty")
+      end
+      if title.present? && !title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+        errors.add(:title, "this title is not clickbait")
+      end
+    end
 end
